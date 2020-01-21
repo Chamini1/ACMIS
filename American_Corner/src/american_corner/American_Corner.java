8de4d8188fa680882c7750a54abbd5d1e89120ca
ssathsara <49017892+ssathsara@users.noreply.github.com>
2020-01-21 13:25:26 +0530

    Add files via upload

---
+++ American_Corner/src/american_corner/American_Corner.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package american_corner;
+
+/**
+ *
+ * @author SATHSARA
+ */
+public class American_Corner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
